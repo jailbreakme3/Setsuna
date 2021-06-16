e4e0d3acffc16a4262b4192b0a0495294e4092c4
jailbreakme3 <jailbreakme3@qq.com>
2021-06-16 18:44:08 +0800

    demo update

--- app/src/main/java/com/jailbreakme3/setsuna/MainActivity.kt
+++ app/src/main/java/com/jailbreakme3/setsuna/MainActivity.kt
@@ -7,6 +7,7 @@ import com.jailbreakme3.setsuna.setsuna.Setsuna
 import com.jailbreakme3.setsuna.setsuna.SetsunaCoroutineScope
 import com.jailbreakme3.setsuna.setsuna.get
 import kotlinx.coroutines.*
+import okhttp3.OkHttpClient
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,8 +15,8 @@ class MainActivity : AppCompatActivity() {
 
     companion object {
         // open api
-        private val APP_ID = "app_id" to "pqlqfhmustlmrrek"
-        private val APP_SECRET = "app_secret" to "cU56aTl1WS9rV3FrQjRzMFE5MTZkUT09"
+        private val APP_ID = "app_id" to "xxxxxxx"
+        private val APP_SECRET = "app_secret" to "xxxxxxxx"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,7 +26,16 @@ class MainActivity : AppCompatActivity() {
 
         // init Setsuna
         Setsuna.init {
-            baseUrl("https://www.mxnzp.com/api")
+            // optional
+            baseUrl("https://www.baidu.com/")
+            // customized okHttpClient(optional, when empty, use default client)
+            okHttpClient {
+                OkHttpClient()
+            }
+            converter {
+                // conventer which implements ResponseBodyConverter (optional)
+                GsonConverter.create()
+            }
         }
 
         request()
@@ -41,14 +51,55 @@ class MainActivity : AppCompatActivity() {
                     "content" - "测试内容"
                     "type" - "0"
                 }
+                param(mapOf("key" to "value"))
+                param("key" to "value")
                 // optional grammar
-//                header {
-//                    "app_id" - "pqlqfhmustlmrrek"
-//                    "app_secret" - "cU56aTl1WS9rV3FrQjRzMFE5MTZkUT09"
-//                }
+                header {
+                    "key" - "xxxxx"
+                }
                 header(APP_ID, APP_SECRET)
             }.parseOrThrow<QRCodeData>().apply {
                 this.log()
+            }
+
+            val result = get {
+                url = ""
+            }.parse<QRCodeData>()
+
+            // run in a new thread or corountine
+            Setsuna.get {
+                url = ""
+                params {
+                    "content" - "测试内容"
+                    "type" - "0"
+                }
+                headers {
+                    "key" - "value"
+                }
+
+                onSuccess {
+                    // callback when request succeed
+                    // handle the response yourself
+                    it.body()
+                }
+
+                onSuccessWithParse<QRCodeData> {
+                    // callback when request succeed
+                    // get the response with a parsed nullable entity
+                }
+
+                // optional below
+                onStart {
+                    // callback when request start
+                }
+
+                onFinish {
+                    // callback when request finished
+                }
+
+                onFail {
+                    // callback when request failed
+                }
             }
 
         }
